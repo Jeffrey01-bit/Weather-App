d35ff3e186e88cfcd4a3eeeca56bdebe354a91f2
Jeffrey01-bit <goldenjeffrey8@gmail.com>
2025-08-30 20:19:04 +0530

    Weather Icons and contents

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Weather.css";
 import searchIcon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
 
 const Weather = () => {
   return (
@@ -9,6 +16,9 @@ const Weather = () => {
         <input type="text" placeholder="Search" />
         <img src={searchIcon} alt="" />
       </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p>16°C</p>
+      <p>London</p>
     </div>
   );
 };
